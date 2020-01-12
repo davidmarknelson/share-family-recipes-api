5b22b37f08db470a0ebf9c5d417a84acdbf40529
David Nelson <nelsondavidmark@gmail.com>
2020-01-12 18:35:51 -0500

    Update email template to include a slash.

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -4,7 +4,7 @@ module.exports = {
     return `
       <h3>Thank you for signing up, ${firstName} ${lastName}.</h3>
       <p>Please verify your email by clicking the link below</p>
-      <a href="${url}verify?token=${verificationToken}">Verify your email</a>
+      <a href="${url}/verify?token=${verificationToken}">Verify your email</a>
       <p>This link will expire in 2 hours.</p>
     `;
   },
@@ -13,7 +13,7 @@ module.exports = {
       <p>You are receiving this because you have requested to reset the password for your account.</p>
       <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
       <p>Please click to button bellow to reset your password.</p>
-      <a href="${url}login/forgotpassword/reset?token=${verificationToken}">Reset your password</a>
+      <a href="${url}/login/forgotpassword/reset?token=${verificationToken}">Reset your password</a>
       <p>This link will expire in 2 hours.</p>
     `;
   }
