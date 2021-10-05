5b451d39973111906ccb388dd0b934cb7bb41479
David Nelson <nelsondavidmark@gmail.com>
2021-10-05 13:06:15 -0500

    Testing.

--- src/controllers/searches.js
+++ src/controllers/searches.js
@@ -41,7 +41,7 @@ module.exports = {
       res.status(200).json(meals);
 
     } catch (err) {
-      res.status(500).json({ message: errorMessage, err });
+      res.status(500).json({ message: errorMessage, err: err });
     }
   },
 
